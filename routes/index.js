d89e0eacabd4f9061191583155e5af2dee536b62
bdzanko17 <bdzanko1@gmail.com>
2019-12-23 22:57:00 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+// //var mysql = require('mysql');
+//
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: ""
+// });
+//
+// con.connect(function (err) {
+// if(err)throw err;
+// console.log("Connected");
+// var sql = "INSERT INTO "
+//
+// });
 
 var globalna = "Alarm je ugasen";
 
@@ -15,6 +29,9 @@ router.get('/2', function(req, res, next) {
   res.send(globalna);
 });
 
+router.get('/3', function(req, res, next) {
+
+});
 
 
 module.exports = router;
