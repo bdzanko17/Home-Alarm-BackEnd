114064be6c3c84d0b4440668c1d5e999425c7995
Benjamin <bdzanko1@gmail.com>
2020-01-05 19:42:01 +0100

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,27 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
+//Konekcija sa bazom
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bmt"
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "bmt"
 });
 
 con.connect(function (err) {
-if(err)throw err;
-console.log("Connected");
-
+    if (err) throw err;
+    console.log("Connected");
 });
 
 var status = "Alarm je ugasen";
 
-/* GET home page. */
+//Get status of alarmm
 router.get('/', function (req, res, next) {
     res.send(status);
 });
 
+//Get history from database
 router.get('/baza', function (req, res, next) {
     con.query("SELECT * FROM detektovano", function (err, result, fields) {
         if (err) throw err;
@@ -29,20 +30,22 @@ router.get('/baza', function (req, res, next) {
     });
 });
 
+
+//Route for alarming server, sent from arduino !
 router.get('/2', function (req, res, next) {
     status = "Alarm je Upaljen";
+    var ubaci ="INSERT INTO detektovano () VALUES  () ";
+    con.query(ubaci, function (err,result) {
+        if(err) throw err;
+    });
     res.send(status);
 });
 
+//Turning off alarm, sent from fronted !
 router.post('/ugasi', function (req, res, next) {
     status = "Alarm je ugasen";
-    let object = {stat: status}
+    let object = {stat: status} //json
     res.send(object);
 });
-
-router.get('/3', function (req, res, next) {
-
-});
-
 
 module.exports = router;
